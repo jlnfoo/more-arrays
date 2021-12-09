02c40b7345ce2e1d2cacbfd767c055df172c5fd6
Jaline Foo <jaline.foo@foodpanda.com>
2021-12-09 14:47:28 +0800

    q2

--- q2.js
+++ q2.js
@@ -8,3 +8,29 @@ i.e.
 // [LOG]: Yorker Hogbin - Transportation - VTWG - 86
 
 */
+// loops each person object
+for (let i = 0; i < mockData.length; i++) {
+  // loops each person's portfolio
+  for (let j = 0; j < mockData[i].portfolio.length; j++) {
+    //loops through each holdings/stocks
+    for (let k = 0; k < mockData[i].portfolio[j].holdings.length; k++) {
+      if (!mockData[i].portfolio[j].sectorIsProfitable) {
+        break;
+      } else if (
+        mockData[i].portfolio[j].sectorIsProfitable &&
+        mockData[i].portfolio[j].holdings[k].isProfitable
+      ) {
+        const profitablePortfolioNames =
+          `${mockData[i].firstName} ` + `${mockData[i].lastName}`;
+        const profitablePortfolioSector = `${mockData[i].portfolio[j].sector}`;
+
+        console.log(
+          "Profitable portfolio : " +
+            profitablePortfolioNames +
+            " - " +
+            profitablePortfolioSector
+        );
+      }
+    }
+  }
+}
